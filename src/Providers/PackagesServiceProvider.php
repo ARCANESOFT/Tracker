3b81a9b7c2ed5aab1c15fcb9be7660d85cb54e2d
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-12-08 20:10:04 +0000

    Updating the PackagesServiceProvider

--- src/Providers/PackagesServiceProvider.php
+++ src/Providers/PackagesServiceProvider.php
@@ -1,5 +1,6 @@
 <?php namespace Arcanesoft\Tracker\Providers;
 
+use Arcanedev\LaravelTracker\LaravelTrackerServiceProvider;
 use Arcanedev\Support\ServiceProvider;
 
 /**
@@ -14,8 +15,42 @@ class PackagesServiceProvider extends ServiceProvider
      |  Main Functions
      | ------------------------------------------------------------------------------------------------
      */
+    /**
+     * Register the service provider.
+     */
     public function register()
     {
-        $this->app->register(\Jenssegers\Agent\AgentServiceProvider::class);
+        $this->registerLaravelTrackerPackage();
+    }
+
+    /**
+     * Boot the service provider.
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Other Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Register Laravel Tracker package.
+     */
+    private function registerLaravelTrackerPackage()
+    {
+        $this->app->register(LaravelTrackerServiceProvider::class);
     }
 }
